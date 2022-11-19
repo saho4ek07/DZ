6eaf4fe51cec7165b6bc1ba33a0df43e75953938
Rostislav <rbelenkin@mail.ru>
2022-11-19 04:55:32 +0300

    сделал 6 задачу

---
+++ zadacha_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num = number;
+
+
+if (num % 2 == 1)
+ {
+    Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+ }
+else
+{
+    Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+}
