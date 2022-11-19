24f6f72bb1530b55ff8374216cfb40d6606ed4cf
Rostislav <rbelenkin@mail.ru>
2022-11-19 05:11:28 +0300

    сделал 8 задачу

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
