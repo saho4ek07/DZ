bf713377765b2b89deb4d896d6dc01ada4445007
Rostislav <rbelenkin@mail.ru>
2022-11-19 04:19:35 +0300

    Сделал два задания

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numberFirst > numberSecond)
+{
+    Console.WriteLine($"{numberFirst} больше чем {numberSecond} ");
+}
+else
+{
+    Console.WriteLine($"{numberSecond} больше чем {numberFirst} ");
+}
---
+++ zadacha_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+int max = numberFirst;
+
+
+if (numberSecond>max)max=numberSecond;
+if (numberThird>max)max=numberThird;
+
+Console.WriteLine($"Наибольшее число {max} ");
